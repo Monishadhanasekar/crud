f0a7a00fe9fac4630ddb420e4630fed05b7e2024
Monishadhanasekar <sekarmonisha2@gmail.com>
2022-03-30 22:39:21 +0530

    files added

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+        Band is a multidisciplinary creative studio based in the Pacific Northwest.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
